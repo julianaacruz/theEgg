539f9f787eceea396734ce39f9966337dcee762b
julianaacruz <juliana201380cc@gmail.com>
2020-09-28 21:30:52 -0500

    se cargan los demás elementos de la composición

--- src/view/Main.java
+++ src/view/Main.java
@@ -13,6 +13,9 @@ public class Main extends PApplet {
 	static PImage lincoln;
 	static PImage hitler;
 	static PImage jesus;
+	static PImage egg;
+	static PImage johnWB;
+	static PImage people;
 
 
 
@@ -42,7 +45,9 @@ public class Main extends PApplet {
 		lincoln = loadImage ("Lincoln.png");
 		hitler = loadImage ("Hitler.png");
 		jesus = loadImage ("Jesus.png");
-
+		egg = loadImage ("Egg.png");
+		johnWB = loadImage ("JohnWB.png");
+		people = loadImage ("People.png");
 
 		arrayStory = loadStrings ("../data/TheEgg.txt");
 		text.controllerList(arrayStory);
@@ -52,20 +57,30 @@ public class Main extends PApplet {
 	
 	
 	public void draw() {
+		tint(255, 255);
 		float f=(float) 0.8;
 		bg1.loadPixels();
 
 		scale(f);
 		image (bg1,800,540);
+		
+		element.drawElement(800, 540, man);
+		image (god,710,540);
+		image (man,900,540);
+		tint(255, 250);
+		//image (johnWB,850,540);
+		//image (people,700,540);
+		//image (people,820,540);
+
+		tint(255, 255);
+		image (lincoln,800,540);
+		image (hitler,700,540);
+		image (jesus,590,540);
+		//image (egg,750,540);
 		bg.parallaxEffect(mouseX,795,-1, bg2);
 		bg.parallaxEffect(mouseX,830,-5, bg3);
 		bg.parallaxEffect(mouseX,955,-35, bg4);
-		element.drawElement(800, 540, man);
-		image (god,760,540);
-		image (man,950,540);
-		image (lincoln,850,540);
-		image (hitler,750,540);
-		image (jesus,640,540);
+
 
 	}
 	
